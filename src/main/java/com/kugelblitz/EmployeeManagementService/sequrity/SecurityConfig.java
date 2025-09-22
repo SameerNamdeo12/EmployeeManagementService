abfdd912ee29e7dc3e70e8c6922723826af6aac1
SameerNamdeo12 <sameernamdeo6@gmail.com>
2025-09-22 15:04:09 +0530

    add security

---
+++ src/main/java/com/kugelblitz/EmployeeManagementService/sequrity/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.kugelblitz.EmployeeManagementService.sequrity;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+}
